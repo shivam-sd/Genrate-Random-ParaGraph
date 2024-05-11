d7c342bbfb2c91a6b485407b0636e2de0bfbd314
Shivam Maurya <sm5708251@gmail.com>
2024-05-11 14:36:08 +0530

    This is random para graph genrater

---
+++ scipt.js
@@ -0,0 +1,47 @@
+let text = [
+    `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, atque corrupti inventore quibusdam dolorum facilis culpa fugit fugiat laboriosam eos minima dolor impedit totam. Porro, iusto officiis. Esse vel dignissimos libero quos, quas officiis dolore perspiciatis ut, id rem minus dolores. Illo laudantium minima dicta accusantium, eligendi ipsum saepe expedita aperiam nisi sed, omnis soluta, impedit ipsam sint possimus corporis distinctio! Sapiente, assumenda nesciunt? Perferendis nulla maxime, iusto ratione obcaecati dignissimos distinctio magni fuga veritatis itaque rem officiis commodi vel, sunt cupiditate quod quas assumenda, quisquam quibusdam animi harum. Nemo architecto perspiciatis facilis commodi laborum illo impedit? Possimus, earum iusto.`,
+
+    `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo, animi laborum neque accusamus molestiae illo et eaque optio nam inventore corporis ratione fugiat rem saepe iste ex officia libero. Iure quam velit ea maxime earum ex corrupti distinctio nam dolorem, quia adipisci dolorum molestiae ad praesentium voluptatum culpa amet nisi vero in illum assumenda dicta neque optio! Ipsam perferendis cumque exercitationem repudiandae voluptate cum harum quasi facere laudantium, saepe, debitis sapiente? Molestiae ducimus consectetur qui. Ipsa quia illo accusamus? Nemo suscipit saepe amet fugit, delectus molestias dolor impedit earum neque.`,
+
+    `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus, reprehenderit repellat sapiente iusto harum nobis ea cupiditate provident sequi rem odit error deleniti, voluptatem minima consequuntur nam qui pariatur inventore hic dicta placeat! Natus molestias, possimus sed veritatis illum obcaecati provident tempore quod voluptatibus velit sapiente quas labore. Officia, voluptas minima praesentium enim deleniti ducimus eos delectus qui placeat fuga accusamus est maiores, harum ipsum molestias nihil quas temporibus in nesciunt perferendis blanditiis autem cum culpa explicabo. Illo ad eos eum quos ipsum eaque fuga voluptates, sint quo cumque quia debitis aut molestias sapiente voluptatum dignissimos quis, suscipit libero sequi sed. Natus reiciendis hic molestiae consequatur distinctio, error aliquam. Et.`,
+
+    `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus, reprehenderit repellat sapiente iusto harum nobis ea cupiditate provident sequi rem odit error deleniti, voluptatem minima consequuntur nam qui pariatur inventore hic dicta placeat! Natus molestias, possimus sed veritatis illum obcaecati provident tempore quod voluptatibus velit sapiente quas labore. Officia, voluptas minima praesentium enim deleniti ducimus eos delectus qui placeat fuga accusamus est maiores, harum ipsum molestias nihil quas temporibus in nesciunt perferendis blanditiis autem cum culpa explicabo. Illo ad eos eum quos ipsum eaque fuga voluptates, sint quo cumque quia debitis aut molestias sapiente voluptatum dignissimos quis, suscipit libero sequi sed. Natus reiciendis hic molestiae consequatur distinctio, error aliquam. Et.`,
+
+    `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus, reprehenderit repellat sapiente iusto harum nobis ea cupiditate provident sequi rem odit error deleniti, voluptatem minima consequuntur nam qui pariatur inventore hic dicta placeat! Natus molestias, possimus sed veritatis illum obcaecati provident tempore quod voluptatibus velit sapiente quas labore. Officia, voluptas minima praesentium enim deleniti ducimus eos delectus qui placeat fuga accusamus est maiores, harum ipsum molestias nihil quas temporibus in nesciunt perferendis blanditiis autem cum culpa explicabo. Illo ad eos eum quos ipsum eaque fuga voluptates, sint quo cumque quia debitis aut molestias sapiente voluptatum dignissimos quis, suscipit libero sequi sed. Natus reiciendis hic molestiae consequatur distinctio, error aliquam. Et.`,
+
+    `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus, reprehenderit repellat sapiente iusto harum nobis ea cupiditate provident sequi rem odit error deleniti, voluptatem minima consequuntur nam qui pariatur inventore hic dicta placeat! Natus molestias, possimus sed veritatis illum obcaecati provident tempore quod voluptatibus velit sapiente quas labore. Officia, voluptas minima praesentium enim deleniti ducimus eos delectus qui placeat fuga accusamus est maiores, harum ipsum molestias nihil quas temporibus in nesciunt perferendis blanditiis autem cum culpa explicabo. Illo ad eos eum quos ipsum eaque fuga voluptates, sint quo cumque quia debitis aut molestias sapiente voluptatum dignissimos quis, suscipit libero sequi sed. Natus reiciendis hic molestiae consequatur distinctio, error aliquam. Et.`,
+
+    `eaque optio nam inventore corporis ratione fugiat rem saepe iste ex officia libero. Iure quam velit ea maxime earum ex corrupti distinctio nam dolorem, quia adipisci dolorum molestiae ad praesentium voluptatum culpa amet nisi vero in illum assumenda dicta neque optio! Ipsam perferendis cumque exercitationem repudiandae voluptate cum harum quasi facere laudantium, saepe, debitis sapiente? Molestiae ducimus consectetur qui. Ipsa quia illo accusamus? Nemo suscipit saepe amet fugit, delectus molestias dolor impedit earum neque`,
+
+    `ea cupiditate provident sequi rem odit error deleniti, voluptatem minima consequuntur nam qui pariatur inventore hic dicta placeat! Natus molestias, possimus sed veritatis illum obcaecati provident tempore quod voluptatibus velit sapiente quas labore. Officia, voluptas minima praesentium enim deleniti ducimus eos delectus qui placeat fuga accusamus est maiores, harum ipsum molestias nihil quas temporibus in nesciunt perferendis blanditiis autem cum culpa explicabo. Illo ad eos eum quos ipsum eaque fuga voluptates, sint quo cumque quia debitis aut molestias sapiente voluptatum dignissimos quis, suscipit libero sequi sed. Natus reiciendis hic molestiae consequatur distinctio, error aliquam. Et.`,
+
+
+    `ea cupiditate provident sequi rem odit error deleniti, voluptatem minima consequuntur nam qui pariatur inventore hic dicta placeat! Natus molestias, possimus sed veritatis illum obcaecati provident tempore quod voluptatibus velit sapiente quas labore. Officia, voluptas minima praesentium enim deleniti ducimus eos delectus qui placeat fuga accusamus est maiores, harum ipsum molestias nihil quas temporibus in nesciunt perferendis blanditiis autem cum culpa explicabo. Illo ad eos eum quos ipsum eaque fuga voluptates, sint quo cumque quia debitis aut molestias sapiente voluptatum dignissimos quis, suscipit libero sequi sed. Natus reiciendis hic molestiae consequatur distinctio, error aliquam. Et.`,
+
+
+    `ea cupiditate provident sequi rem odit error deleniti, voluptatem minima consequuntur nam qui pariatur inventore hic dicta placeat! Natus molestias, possimus sed veritatis illum obcaecati provident tempore quod voluptatibus velit sapiente quas labore. Officia, voluptas minima praesentium enim deleniti ducimus eos delectus qui placeat fuga accusamus est maiores, harum ipsum molestias nihil quas temporibus in nesciunt perferendis blanditiis autem cum culpa explicabo. Illo ad eos eum quos ipsum eaque fuga voluptates, sint quo cumque quia debitis aut molestias sapiente voluptatum dignissimos quis, suscipit libero sequi sed. Natus reiciendis hic molestiae consequatur distinctio, error aliquam. Et.`,
+]
+
+const input = document.getElementById("input");
+const datacontainer = document.querySelector(".text-container");
+
+function genratepara(){
+document.querySelector("button").addEventListener("click" ,() => {
+    console.log(input.value);
+    if(isNaN(input.value) || input.value < 0 || input.value > 9){
+        const randomdataidx = Math.floor(Math.random() * text.length);
+        datacontainer.innerHTML = `<p> ${text[randomdataidx]} </p>`
+    }
+    else{
+
+  const data = text.slice(0,input.length);
+  const parars = data.map((da) => {
+    return `<p> ${da} </p>`;
+  })
+
+  datacontainer.innerHTML = parars.join("");
+    }
+})
+}
+
+genratepara();
